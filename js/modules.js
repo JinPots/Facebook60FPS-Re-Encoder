fa55e0ac52007025b1090177b5b0c00c487b4413
JinPots <huuthien27012@gmail.com>
2022-03-04 22:26:45 +0700

    Settings and no -crf

--- js/modules.js
+++ js/modules.js
@@ -1,70 +1,67 @@
+/* eslint-disable no-undef */
 const {
-    shell,
-    Notification
-} = require('electron');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec)
-const fs = require('fs');
+	shell,
+	Notification
+} = require('electron')
 const {
-    spawn
-} = require('child_process');
-const path = require('path');
-const fluent = require('fluent-ffmpeg');
-let ffprobe = global.ffprobe
-let ffmpeg = global.ffmpeg
-let store = global.store
-let videoOutputPath = store.get('savePath'),
-    ffmpegProcess
+	spawn
+} = require('child_process')
+let ffmpeg = global.ffmpeg,
+	ffmpegProcess,
+	win = global.win,
+	log = global.log
 
-module.exports.startRender = async function (arg, event, win, dev) {
-    let args = []
+module.exports.startRender = async function (arg) {
+	let args = []
 
-    fluent.setFfprobePath(ffprobe)
-    fluent.setFfmpegPath(ffmpeg)
+	let fileSelect = arg.fileSelect
+	let fileName = arg.fileSelect_name.split('.')[0]
 
-    let fileSelect = arg.fileSelect
-    let fileName = arg.fileSelect_name.split('.')[0]
+	args.push('-i', `${fileSelect.split(/ +/).join('\\ ')}`)
+	if (arg.encoder === 'cpu') {
+		args.push('-c:v', 'libx264')
+	} else if (arg.encoder === 'nvidia') {
+		args.push('-c:v', 'h264_nvenc')
+	} else if (arg.encoder === 'intel') {
+		args.push('-c:v', 'h264_qsv')
+	} else if (arg.encoder === 'amd') {
+		args.push('-c:v', 'h264_amf')
+	}
+	args.push('-qp', '29')
+	args.push('-crf', '15')
+	args.push('-preset', arg.preset)
+	args.push('-c:a', arg.audio)
+	let bitrate = (arg.bitrate / 1000).toFixed(1) + 'M'
+	args.push('-b:v', bitrate)
+	args.push('-pix_fmt', arg.pixel.toLowerCase())
+	args.push('-rc-lookahead', '15')
+	args.push('-vf', `scale=-1:${arg.resolution.split(/ +/).join('')}`)
+	args.push(fileSelect + fileName.split(/ +/).join('\\ ') + '_60fps.mp4')
+	args.push('-y')
+	let currentTime = process.hrtime()
+	console.log(ffmpeg + args.join(' '))
+	ffmpegProcess = spawn(ffmpeg, args)
+	ffmpegProcess.stdout.setEncoding('utf8')
+	ffmpegProcess.stderr.setEncoding('utf8')
+	ffmpegProcess.stdout.on('data', (data) => {
+		log.info(data)
+	})
+	ffmpegProcess.stdout.on('close', (data, error) => {
+		if (error) {
+			console.error(`exec error: ${error}`)
+		}
+		let tempTime = process.hrtime(currentTime)
+		let eplasedTime = (((tempTime[0] * 1000) + (tempTime[1] / 1000000)) / 1000).toFixed(2)
+		win.webContents.send('render-finish', (eplasedTime))
+		shell.showItemInFolder(fileSelect + fileName.split(/ +/).join('\\ ') + '_60fps.mp4')
+		new Notification({
+			title: 'Render finished!',
+			body: 'Render time: ' + eplasedTime + 's'
+		}).show()
+		log.info('Render finished!', 'Render time: ' + eplasedTime + 's')
+	})
+}
 
-    args.push('-i', `${fileSelect.split(/ +/).join('\\ ')}`);
-    if (arg.encoder === 'cpu') {
-        args.push('-c:v', 'libx264');
-    } else if (arg.encoder === 'nvidia') {
-        args.push('-c:v', 'h264_nvenc');
-    } else if (arg.encoder === 'intel') {
-        args.push('-c:v', 'h264_qsv');
-    } else if (arg.encoder === 'amd') {
-        args.push('-c:v', 'h264_amf');
-    }
-    args.push('-qp', '29');
-    args.push('-crf', '15');
-    args.push('-preset', arg.preset);
-    args.push('-c:a', arg.audio);
-    let bitrate = (arg.bitrate / 1000).toFixed(1) + 'M';
-    args.push('-b:v', bitrate);
-    args.push('-pix_fmt', arg.pixel.toLowerCase());
-    args.push('-rc-lookahead', '15');
-    args.push('-vf', `scale=-1:${arg.resolution.split(/ +/).join('')}`);
-    args.push(videoOutputPath + fileName.split(/ +/).join('\\ ') + '_60fps.mp4');
-    args.push('-y');
-    let currentTime = process.hrtime()
-    console.log(ffmpeg + args.join(' '));
-    ffmpegProcess = spawn(ffmpeg, args)
-    ffmpegProcess.stdout.setEncoding('utf8');
-    ffmpegProcess.stderr.setEncoding('utf8');
-    ffmpegProcess.stdout.on(`data`, (data) => {
-        console.log(data);
-    });
-    ffmpegProcess.stdout.on(`close`, (data, error) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-        }
-        let tempTime = process.hrtime(currentTime)
-        let eplasedTime = (parseFloat(tempTime)).toFixed(2)
-        win.webContents.send('render-finish', (eplasedTime));
-        shell.openPath(videoOutputPath);
-        new Notification({
-            title: 'Render finished!',
-            body: 'Render time: ' + eplasedTime + "s"
-        }).show();
-    })
+module.exports.sendData = function (channel, args) {
+	win.webContents.send(channel, args)
 }
--- js/script.js
+++ js/script.js
@@ -1,55 +1,81 @@
+/* eslint-disable no-unused-vars */
 function startRender(event) {
-    event.preventDefault() // stop the form from submitting
-    const formData = {};
-    const form = document.querySelector('form');
-    const elements = form.elements; // all form elements
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements.item(i);
-        if (element.id) {
-            formData[element.id] = element.value;
-        }
-        if (element.className === 'fileSelect') {
-            formData[element.className] = element.files[0].path;
-            formData[element.className + '_name'] = element.files[0].name;
-        }
-    }
-    
-    const progress = document.getElementById('progress');
+	event.preventDefault() // stop the form from submitting
+	const formData = {}
+	const form = document.querySelector('form')
+	const elements = form.elements // all form elements
+	for (let i = 0; i < elements.length; i++) {
+		const element = elements.item(i)
+		if (element.id) {
+			formData[element.id] = element.value
+		}
+		if (element.className === 'fileSelect') {
+			formData[element.className] = element.files[0].path
+			formData[element.className + '_name'] = element.files[0].name
+		}
+	}
 
-    progress.style.display = 'block';
-    progress.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Rendering...';
-    progress.style.color = 'black';
+	const progress = document.getElementById('progress')
 
-    // send formData to main process and start render
-    window.api.send("render", formData);
+	progress.style.display = 'block'
+	progress.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Rendering...'
+	progress.style.color = 'black'
 
-    // recieve message when render is finished and open the folder
-    window.api.receive("render-finish", (event) => {
-        progress.innerText = `Render finished`;
-        progress.style.color = '#00ff00';
-    });
+	window.backend.send('render', formData)
+
+	window.backend.receive('render-finish', (eplasedTime) => {
+		progress.innerText = `Render finished in ${eplasedTime} seconds!`
+		progress.style.color = '#00ff00'
+	})
 }
 
 function openVideosFolder() {
-    window.api.send("openVideosFolder", null);
+	window.backend.send('openVideosFolder', null)
 }
 
-let file = document.querySelector('#file-select > input');
+let file = document.querySelector('#file-select > input')
 
-document.querySelector("#submit").addEventListener('click', () => {
-    if (file.files.length <= 0) {
-        document.querySelector("#file-select > div > p").classList.add('shake');
-        setTimeout(() => {
-            document.querySelector("#file-select > div > p").classList.remove('shake');
-        }, 550);
-    }
-});
-
-function random1() {
-    window.location.href = 'info.html'
-}
-
-window.api.receive('config', (config) => {
-    console.log(config)
+document.querySelector('#submit').addEventListener('click', () => {
+	if (file.files.length <= 0) {
+		document.querySelector('#file-select > div > p').classList.add('shake')
+		setTimeout(() => {
+			document.querySelector('#file-select > div > p').classList.remove('shake')
+		}, 550)
+	}
 })
 
+document.getElementById('settingsb').addEventListener('click', () => {
+	const box = document.getElementById('settings')
+	if (box.style.visibility == 'hidden') {
+		box.style.visibility = 'visible'
+	} else {
+		box.style.visibility = 'hidden'
+	}
+})
+
+async function selectDirs (event) {
+	event.preventDefault()
+	window.backend.send('select-dirs', (event))
+
+	window.backend.receive('select-dirs-a', (data) => {
+		const path = data
+		console.log(path)
+		document.getElementById('pathSettings').setAttribute('value', path)
+	})
+}
+
+window.backend.receive('config', (config) => {
+	console.log(config)
+})
+
+
+function videoNameSubmit (event) {
+	event.preventDefault()
+	const videoName1 = document.getElementById('videoName').getAttribute('value')
+	console.log(videoName1)
+	window.backend.send('videoNameSubmit', (videoName1))
+}
+
+function openLink (url) {
+	window.location.href = url
+}
