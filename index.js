fe49d0e7acaacbcb912dff3451b8e2fb8b511a5f
JinPots <huuthien27012@gmail.com>
2022-03-04 22:25:58 +0700

    Clean shits and user preferences

--- index.js
+++ index.js
@@ -1,115 +1,114 @@
-// setup simple electron app
-const { app, BrowserWindow, ipcMain, shell } = require('electron');
+/* eslint-disable no-undef */
+const { app, BrowserWindow, ipcMain, shell } = require('electron')
 const Store = require('electron-store')
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log')
+const path = require('path')
+const ffmpegPath = require('ffmpeg-static')
 
-const store = new Store();
+
+autoUpdater.logger = log
+autoUpdater.logger.transports.file.level = 'info'
+log.info('App starting...')
+global.log = log
+
+const store = new Store()
 global.store = store
+log.info(store.path)
 
-const path = require('path');
-global.shell = shell;
-global.app = app;
+global.shell = shell
+global.app = app
 
 let appPath = app.getAppPath()
 
-const ffmpegPath = require('ffmpeg-static');
-const fs = require('fs');
 global.ffmpeg = ffmpegPath.replace('app.asar', 'app.asar.unpacked') + ' '
-let win;
+let win
 
-// Keep a reference for dev mode
-let dev = false;
+// Keep a reference for dev mode    
+let dev = false
 
 // Check if running in development or production
 if (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath)) {
-    dev = true;
+	dev = true
 }
+
+global.dev = dev
 
 if (dev == false) {
-    appPath.replace('app.asar', 'app.asar.unpacked')
+	appPath.replace('app.asar', 'app.asar.unpacked')
 }
 
-let savePath = store.get('savePath')
-if (!savePath) {
-    store.set('savePath', `${process.env.USERPROFILE}\\Videos\\`)
-}
+store.get('path', `${process.env.USERPROFILE}\\Videos\\`)
+
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 1200,
-        icon: "./assets/icon.ico",
-        height: 700,
-        webPreferences: {
-            // Preload script
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true
-        },
-        fullscreen: false,
-        fullscreenable: false,
-        maximizable: false,
-        resizable: false
-    });
-    // win.removeMenu()
-    win.loadFile('web/index.html')
+	win = new BrowserWindow({
+		width: 1200,
+		icon: './assets/icon.ico',
+		height: 700,
+		webPreferences: {
+			// Preload script
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+			contextIsolation: true
+		},
+		fullscreen: false,
+		fullscreenable: false,
+		maximizable: false,
+		resizable: false
+	})
+	if (dev == false) {
+		win.removeMenu()
+	}
+	win.loadFile('web/index.html')
+	autoUpdater.checkForUpdatesAndNotify()
+	global.win = win
 }
 
-// Check if ffmpeg is installed
-// if not then install
-function checkFFmpeg() {
-    const { exec } = require('child_process');
-    const ffmpegPath = require('ffmpeg-static')
-    exec('ffmpeg -version', (error, stdout, stderr) => {
-        if (error) {
-            console.log('FFmpeg not found');
-            if (dev == false) {            
-                global.ffmpeg = ffmpegPath.replace('app.asar', 'app.asar.unpacked')
-            } else {
-                global.ffmpeg = ffmpegPath
-            }
-        } else {
-            console.log('FFmpeg found');
-            global.ffmpeg = `ffmpeg `
-        }
-    });
-}
 
-function checkFFprobe() {
-    const { exec } = require('child_process');
-    const ffprobePath = require('ffprobe-static').path
-    exec('ffprobe -version', (error, stdout, stderr) => {
-        if (error) {
-            console.log('FFprobe not found');
-
-            if (dev == false) {
-                global.ffmpeg = ffprobePath.replace('app.asar', 'app.asar.unpacked')
-            } else {
-                global.ffmpeg = ffprobePath
-            }
-        } else {
-            console.log('FFprobe found');
-            global.ffprobe = `ffprobe `
-        }
-    });
-}
 
 app.whenReady().then(async () => {
-    // Check if ffmpeg is installed
-    // checkFFmpeg()
-    // checkFFprobe()
-    createWindow()
+	createWindow()
 
-    // Set AppUserModelId
+	// Set AppUserModelId
+	if (process.platform === 'win32') {
+		app.setAppUserModelId('Facebook 60FPS Re-encoder')
+	}
 
-    if (process.platform === "win32") {
-        app.setAppUserModelId("Facebook 60FPS Re-encoder")
-    }
-
+	const { sendData } = require('./js/modules')
+	const data = {}
+	data.path = store.get('path', `${process.env.USERPROFILE}\\Videos\\`)
+	sendData('config', (data))
 })
 
+
 ipcMain.on('render', async (event, args) => {
-    const { startRender } = require('./js/modules');
-    await startRender(args, event, win, dev);
-});
+	const { startRender } = require('./js/modules')
+	await startRender(args, event)
+})
 
 ipcMain.on('openVideosFolder', () => {
-    shell.openPath(`${process.env.USERPROFILE}\\Videos\\`);
+	shell.openPath(`${process.env.USERPROFILE}\\Videos\\`)
+})
+
+const { dialog } = require('electron')
+
+ipcMain.on('openLink', (url) => {
+	shell.openExternal(url)
+})
+
+ipcMain.on('select-dirs', () => {
+	const { sendData } = require('./js/modules')
+	dialog.showOpenDialog(win, {
+		properties: ['openDirectory']
+	}).then((data) => {
+		const result = `${data.filePaths[0]}`
+		store.set('path', `${result}`)
+		sendData('select-dirs-a', (result))
+	})
+})
+
+ipcMain.on('videoNameSubmit', (data) => {
+	// log.info(data)
+	store.set('videoName', `${data}`)
 })
