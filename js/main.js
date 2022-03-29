a430e5bc0fe7fe4b0e7f3c609cde32f5426d0b1e
sora <34794115+skyventuree@users.noreply.github.com>
2022-03-30 01:04:03 +0700

    Overhauled UI changes

---
+++ js/main.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-unused-vars */
+let sbb = document.querySelector('#submit')
+if (sbb) {
+	sbb.addEventListener('click', () => {
+		if (document.querySelector('#file-select > input').files.length <= 0) {
+			document.querySelector('#file-select > div > p').classList.add('shake')
+			setTimeout(() => {
+				document.querySelector('#file-select > div > p').classList.remove('shake')
+			}, 550)
+		}
+	})
+}
+
+let stbb = document.getElementById('settingsb')
+if (stbb) {
+	stbb.addEventListener('click', () => {
+		const box = document.getElementById('settings')
+		if (box.style.visibility == 'hidden') {
+			box.style.visibility = 'visible'
+		} else {
+			box.style.visibility = 'hidden'
+		}
+	})
+}
+
+async function selectDirs(event) {
+	event.preventDefault()
+	window.backend.send('select-dirs', (event))
+	window.backend.receive('select-dirs-a', (data) => {
+		if (data == "undefined") return;
+		console.log(data)
+		document.getElementById('pathSettings').setAttribute('value', data || '')
+	})
+	document.getElementById('pathSettings').blur();
+}
+
+function videoNameSubmit(event) {
+	event.preventDefault()
+	const videoName1 = document.getElementById('videoName').getAttribute('value')
+	console.log(videoName1)
+	window.backend.send('videoNameSubmit', (videoName1))
+}
+
+const resolutionChange = () => {
+	const resolution = document.getElementById("resolution").value;
+	if (resolution === "720") {
+		document.getElementById("qp").value = 24;
+		document.getElementById("crf").value = 8;
+	} else if (resolution === "1080") {
+		document.getElementById("qp").value = 21;
+		document.getElementById("crf").value = 5;
+	}
+}
+
+function addVideo() {
+	document.querySelector("#backdrop").classList.add("active");
+	document.querySelector("#credit").style.color = "white";
+	document.querySelectorAll("#credit>p>a").forEach(function(e) {
+		e.style.color = "lightblue";
+	});
+	
+	// set title
+	const video = document.querySelector('#file-select > input');
+	document.querySelector("#preview > p").innerHTML = "File selected: " + video.files[0].name;
+}
+
+function cancelVideo() {
+	document.querySelector("#backdrop").classList.remove("active");
+	document.querySelector("#credit").style.color = "black";
+	document.querySelectorAll("#credit>p>a").forEach(function(e) {
+		e.style.color = "#007bff";
+	});
+	document.querySelector('#advanced').style.display = 'none';
+	document.querySelector('#file-select > input').value = '';
+	document.getElementById('pathSettings').setAttribute('value', '')
+	document.querySelector("#progress").style.display = "none";
+}
+
+// aka toggle advanced settings
+function toggleRibbon() {
+	const advancedSetings = document.querySelector('#advanced');
+	if (advancedSetings.style.display == 'none') {
+		advancedSetings.style.display = 'block';
+		return;
+	}
+	advancedSetings.style.display = 'none';
+}
---
+++ js/render.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+function startRender(event) {
+	event.preventDefault() // stop the form from submitting
+	const formData = {}
+	const form = document.querySelector('form')
+	const elements = form.elements // all form elements
+	for (let i = 0; i < elements.length; i++) {
+		const element = elements.item(i)
+		if (element.id) {
+			formData[element.id] = element.value
+		}
+		if (element.className === 'fileSelect') {
+			formData[element.className] = element.files[0].path
+			formData[element.className + '_name'] = element.files[0].name
+		}
+	}
+
+	const progress = document.getElementById('progress')
+	progress.style.display = 'flex'
+
+	window.backend.send('render', formData)
+
+	window.backend.receive('render-progress', (data) => {
+		console.log(data)
+	})
+
+	window.backend.receive('render-finish', (eplasedTime) => {
+		if (eplasedTime === 'error') {
+			const errorBox = document.getElementById('error-dialog')
+			errorBox.style.display = 'flex';
+			progress.style.display = 'none';
+			progress.innerHTML = '<i class="fa fa-exclamation-triangle"></i>'
+		} else {
+			document.querySelector("#progress > i").classList.remove('fa-circle-o-notch')
+			document.querySelector("#progress > i").classList.add('fa-check-circle')
+			document.querySelector("#progress > h2").innerHTML = 'Finished!'
+			document.querySelector("#summary").style.display = 'flex'
+			document.querySelector("#summary > p") = `Render finished in ${eplasedTime} seconds!`
+		}
+	})
+}
+
+function openVideosFolder() {
+	window.backend.send('openVideosFolder', null)
+}
